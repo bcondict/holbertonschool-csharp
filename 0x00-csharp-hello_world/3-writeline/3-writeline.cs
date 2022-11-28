41865da510effff5722df9e30fd6b967a5d1ea37
Jesus <jesus.junc10@gmail.com>
2022-11-28 13:29:44 -0500

    3 task complete

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,1 +1,11 @@
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+using Sistem;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
