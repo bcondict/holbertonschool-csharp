97568947ec884c59fcd6ea446b664e55d19a8121
Jesus <jesus.junc10@gmail.com>
2022-11-28 13:49:04 -0500

    3 test 0

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -3,6 +3,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        Console.Out.WriteLine("\"Programming is like building a multilingual puzzle");
     }
 }
