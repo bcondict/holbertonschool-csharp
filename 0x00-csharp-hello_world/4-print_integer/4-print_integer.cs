5cdb4ae6525ad89b426ac733ab241f90bd057720
Jesus <jesus.junc10@gmail.com>
2022-11-28 15:04:01 -0500

    past task correction

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -3,8 +3,8 @@
 class Program
 {
 	static void Main(string[] args)
-    {
-		float number = 3.14159f;
+	{
+		int number = 972;
         Console.WriteLine("{0} Mission Street", number);
-    }
+	}
 }
