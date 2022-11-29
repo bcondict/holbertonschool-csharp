fbd80702cae64af4eba5ce2696f06ceb44efc7c7
Jesus <jesus.junc10@gmail.com>
2022-11-29 16:31:47 -0500

    1 task ?: operator

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int LastDigit = number % 10;
+
+        Console.WriteLine("The las digit of {0} is {1} and is {2}", number, LastDigit,
+        LastDigit > 5 ? "greater than 5" : ( LastDigit == 0 ? "0" : "less than 6 and not 0"));
+    }
+}
