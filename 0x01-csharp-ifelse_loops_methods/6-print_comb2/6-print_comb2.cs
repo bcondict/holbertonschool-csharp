c25d6e780e4444dc42b4ece0134ee417d8f8b26a
Jesus <jesus.junc10@gmail.com>
2022-11-29 17:20:52 -0500

    6 task

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 8 && j != i; j++)
+                {
+                    if (i == 9 && j == 8)
+                    {
+                        Console.WriteLine("89");
+                    }
+                    else
+                    {
+                        Console.Write(j);
+                        Console.Write("{0}, ", i);
+                    }
+                }
+            }
+        }
+    }
+}
