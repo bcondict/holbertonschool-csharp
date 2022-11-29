e70ffcfb2a0a2f08e3663f72d1602e4e9e8005fa
Jesus <jesus.junc10@gmail.com>
2022-11-29 16:32:58 -0500

    1 task text correction

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -8,7 +8,7 @@ class Program
         int number = rndm.Next(-10000, 10000);
         int LastDigit = number % 10;
 
-        Console.WriteLine("The las digit of {0} is {1} and is {2}", number, LastDigit,
+        Console.WriteLine("The last digit of {0} is {1} and is {2}", number, LastDigit,
         LastDigit > 5 ? "greater than 5" : ( LastDigit == 0 ? "0" : "less than 6 and not 0"));
     }
 }
