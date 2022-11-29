1b7401a291b631f02bc76583e9093bd4519382bc
Jesus <jesus.junc10@gmail.com>
2022-11-29 18:20:49 -0500

    11 task

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int j = 0;
+        if (length >= 0)
+            Console.WriteLine();
+        for (int i = 0; i < length; i++)
+        {
+            while (j < i);
+            {
+                Console.WriteLine(" ");
+                j++;
+            }
+            Console.WriteLine("\\")
+        }
+    }
+}
