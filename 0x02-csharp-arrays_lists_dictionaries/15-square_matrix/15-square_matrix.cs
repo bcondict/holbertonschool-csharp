90c8267ba6ad773be2e02e087b52c464880ed508
Jesus <jesus.junc10@gmail.com>
2022-11-30 12:52:27 -0500

    15 task

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int[,] square_matrix = myMatrix.Clone() as int[,];
+        for (int i = 0; i < myMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < myMatrix.GetLength(1); j++)
+            {
+                square_matrix[i, j] = myMatrix[i, j] * myMatrix[i, j];
+            }
+        }
+        return square_matrix;
+    }
+}
