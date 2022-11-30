204e01cc13fcd85da666b69114a3b0161204b6cc
Jesus <jesus.junc10@gmail.com>
2022-11-30 12:14:34 -0500

    14 task

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr2d = new int[5,5];
+            arr2d[2, 2] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    if (j != 4)
+                        Console.Write(arr2d[i, j] + " ");
+                    else
+                        Console.Write(arr2d[i, j]);
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
