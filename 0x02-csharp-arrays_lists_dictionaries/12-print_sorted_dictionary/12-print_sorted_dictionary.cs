ea238e82c5a69df2d970dd121c2bd93dc7925bd8
Jesus <jesus.junc10@gmail.com>
2022-11-30 11:29:29 -0500

    12 task

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sorted_key = from entry in myDict orderby entry.Key ascending select entry;
+        foreach (var key in sorted_key)
+        {
+            Console.WriteLine(key.Key + ": " + key.Value);
+        }
+    }
+}
