8e9ef57d4a50f996fc833ee301ef186b846c583b
Jesus <jesus.junc10@gmail.com>
2022-11-30 09:59:14 -0500

    4 task

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if (size == 0)
+            Console.WriteLine();
+
+        List<int> new_list = new List<int>();
+        new_list = new List<int>(size);
+        for (int i = 0; i < size; i++)
+        {
+            new_list.Add(i);
+            if (i == size - 1)
+            {
+                Console.WriteLine(new_list[i]);
+                break;
+            }
+            Console.Write(new_list[i] + " ");
+        }
+        return new_list;
+    }
+} 
