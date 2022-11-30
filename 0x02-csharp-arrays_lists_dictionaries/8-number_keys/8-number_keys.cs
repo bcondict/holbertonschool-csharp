5ea2f12e3232bce7ffb3bc6b0600c471f4e7c0a0
Jesus <jesus.junc10@gmail.com>
2022-11-30 10:28:43 -0500

    8 task

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        if (myDict == null)
+        {
+            return 0;
+        }
+        int keyCount = 0;
+        foreach (var key in myDict)
+        {
+            keyCount++;
+        }
+        return keyCount;
+    }
+}
