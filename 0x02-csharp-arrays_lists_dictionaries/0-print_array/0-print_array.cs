6be072b6ba9da42fd42099d7111ef79573e570c0
Jesus <jesus.junc10@gmail.com>
2022-11-30 08:58:10 -0500

    0 task

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+            Console.WriteLine();
+
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] new_array = new int[size];
+
+        for (int i = 0; i < size; i++)
+            new_array[i] = i;
+        return new_array;
+    }
+} 
