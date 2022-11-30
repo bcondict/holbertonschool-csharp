aafc0044fac8bf45d307659ca2e13884586296ad
Jesus <jesus.junc10@gmail.com>
2022-11-30 12:30:50 -0500

    5 task correction

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -6,7 +6,10 @@ class List
     public static int MaxInteger(List<int> myList)
     {
         if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
             return -1;
+        }
 
         int max = myList[0];
         for (int i = 1; i < myList.Count; i++)
