621da7cd1163c59897dde15ce0ed72ab98a92ae5
Jesus <jesus.junc10@gmail.com>
2022-11-30 11:49:19 -0500

    13 task

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+            return "None";
+        var sorted_val = from entry in myList orderby entry.Value descending select entry;
+        return (sorted_val.ElementAt(0).Key);
+
+    }
+}
