b8114a86187f33f70167db03d0f1ff3467e49974
Jesus <jesus.junc10@gmail.com>
2022-11-30 12:33:55 -0500

    2 task tab correcton

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -5,10 +5,10 @@ class Array
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
         if (index < 0 || index >= array.Length)
-            {
-                Console.WriteLine("Index out of range");
-                return array;
-            }
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
 
         array[index] = n;
         return array;
