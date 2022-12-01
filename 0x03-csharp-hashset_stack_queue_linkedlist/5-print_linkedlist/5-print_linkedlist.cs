37da2ff859634f4ea9c9de0ce4c389db555b672a
Jesus <jesus.junc10@gmail.com>
2022-12-01 14:42:32 -0500

    5 task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> MyLinkedList = new LinkedList<int>();
+        if (size < 0)
+            return MyLinkedList;
+
+        for (int i = 0; i < size; i++)
+        {
+            MyLinkedList.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return MyLinkedList;
+    }
+}
