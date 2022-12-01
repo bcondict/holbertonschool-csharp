df0d089739238f1fd1eb1ff0477ab9aa747a0879
Jesus <jesus.junc10@gmail.com>
2022-12-01 13:24:05 -0500

    3 task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: " + aStack.Peek());
+
+        Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search));
+
+        Stack<string> copy = new Stack<string>(aStack);
+
+        if (aStack.Contains(search))
+        {
+            foreach (var item in copy)
+            {
+                aStack.Pop();
+                if (item == search)
+                {
+                    aStack.Pop();
+                    break;
+                } 
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
