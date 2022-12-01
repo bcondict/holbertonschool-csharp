5459bc73b2be83c9182ff0924b6d7ed3ab7c36a1
Jesus <jesus.junc10@gmail.com>
2022-12-01 11:31:38 -0500

    0 task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> result = new HashSet<int>(myList);
+        int sum = 0;
+
+        foreach (var item in result)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+}
