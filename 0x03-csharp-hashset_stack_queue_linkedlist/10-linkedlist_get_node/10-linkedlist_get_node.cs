8051cff74d187d452cb86980c094210812fc6c70
Jesus <jesus.junc10@gmail.com>
2022-12-01 15:12:13 -0500

    10 task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        foreach (var item in myLList)
+        {
+            if (n == i)
+                return item;
+            i++;
+        }
+        return 0;
+    }
+}
