32ea9f15d64d29525be118635cf573a4fd67600d
Jesus <jesus.junc10@gmail.com>
2022-12-01 14:49:46 -0500

    7 task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLlist, int n)
+    {
+        LinkedListNode<int> node = new LinkedListNode<int>(n);
+        myLlist.AddFirst(node);
+
+        return node;
+    }
+}
