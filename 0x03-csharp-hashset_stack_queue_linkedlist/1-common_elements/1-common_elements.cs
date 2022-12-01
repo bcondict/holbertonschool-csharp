c2bba1f2647867eb46e2324d6b1946eed108482b
Jesus <jesus.junc10@gmail.com>
2022-12-01 12:33:29 -0500

    1 task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Sorting
+{
+    public static List<int> GetSortedList(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            for (int j = i + 1; j < list.Count; j++)
+            {
+                if (list[i] > list[j])
+                {
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+        return list;
+    }
+}
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> new_list = new List<int>();
+
+        if (list1 == null || list2 == null)
+        {
+            return new_list;
+        }
+
+        for (int i = 0; i < list1.Count; i++)
+        {
+            if (list2.Contains(list1[i]) && new_list.Contains(list1[i]) == false)
+                new_list.Add(list1[i]);
+        }
+        return Sorting.GetSortedList(new_list);
+    }
+}
