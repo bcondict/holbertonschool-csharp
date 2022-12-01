6b7d552390268f1d448a64427f95bd58f926c77a
Jesus <jesus.junc10@gmail.com>
2022-12-01 14:46:37 -0500

    6 task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList 
+{
+    public static int Length(LinkedList<int> myLlist)
+    {
+        int count = 0;
+        foreach (int item in myLlist)
+        {
+            count++;
+        }
+        return count;
+    }
+}
