2d3f5d69f25c91949437e1552bbd5980410c7075
Jesus <jesus.junc10@gmail.com>
2022-12-04 21:39:12 -0500

    all main files:

---
+++ 0x02-csharp-arrays_lists_dictionaries/main/9-main.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> myDict = new Dictionary<string, string>();
+        string key;
+        string value;
+
+        key = "school";
+        value = "Holberton";
+
+        Dictionary.AddKeyValue(myDict, key, value);
+
+        foreach (KeyValuePair<string, string> entry in myDict)
+            Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+
+        Console.WriteLine("------------------");
+
+        key = "city";
+        value = "San Francisco";
+
+        Dictionary.AddKeyValue(myDict, key, value);
+
+        foreach (KeyValuePair<string, string> entry in myDict)
+            Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+
+        Console.WriteLine("------------------");
+
+        key = "school";
+        value = "hola";
+
+        Dictionary.AddKeyValue(myDict, key, value);
+
+        foreach (KeyValuePair<string, string> entry in myDict)
+            Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+
+        Console.WriteLine("------------------");
+    }
+}
