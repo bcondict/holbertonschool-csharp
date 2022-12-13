a1527d47561935328df0b331cf74cf96d0a99aea
Jesus <jesus.junc10@gmail.com>
2022-12-13 09:23:41 -0500

    1 task

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            if (b == 0)
+                Console.WriteLine("{0} / {1} = 0", a, b);
+            else
+                Console.WriteLine("{0} / {1} = {2}", a, b, a/b);
+        }
+    }
+}
