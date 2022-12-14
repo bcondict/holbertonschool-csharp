67ddf65569dac5f4e802f01de76234edc496497a
Jesus <jesus.junc10@gmail.com>
2022-12-14 14:56:28 -0500

    1 task

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enemies 
+{
+    /// <sumary>
+    /// Define an enemy, Zombie with it's characteristics
+    /// </sumary>
+
+    public class Zombie
+    {
+        /// <sumary>health field of Zombie </sumary>  
+        public int health;
+
+        /// <sumary>Constructor to <see cref="Zombie"/> class.</sumary> 
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
