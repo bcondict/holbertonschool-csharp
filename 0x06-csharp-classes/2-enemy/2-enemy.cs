53cca800843352f661744fc99d43a1c5a4794580
Jesus <jesus.junc10@gmail.com>
2022-12-14 15:03:12 -0500

    2 task

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enemies 
+{
+    /// <sumary>
+    /// Define an enemy, Zombie with it's characteristics
+    /// </sumary>
+
+    public class Zombie
+    {
+        /// <sumary>health field of Zombie </sumary>  
+        public int health;
+
+        /// <sumary>Constructor to <see cref="Zombie"/> class.</sumary> 
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <sumary>Constructor to <see cref="Zombie"/> class that sets heat to a value.</sumary> 
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                health = value;
+                return;
+            }
+            thow new ArgumentException("Health must be greater than or equal to 0");
+        }
+    }
+}
