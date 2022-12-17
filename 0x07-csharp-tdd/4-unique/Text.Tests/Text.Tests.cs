369f2ba5eff83b379fbcf82c5ba499396b1cd60d
Jesus <jesus.junc10@gmail.com>
2022-12-16 23:26:26 -0500

    4 task

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -1,18 +1,56 @@
 using NUnit.Framework;
+using Text;
 
 namespace Text.Tests
 {
+	[TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Unique()
         {
+			string test = "uniq";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(0, result);
         }
 
         [Test]
-        public void Test1()
+        public void UniqueNotUnique()
         {
-            Assert.Pass();
+			string test = "hiahiahia";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void UniqueEmpty()
+        {
+			string test = "";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void UniqueOneChar()
+        {
+			string test = "h";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void UniqueLast()
+        {
+			string test = "hihililia";
+			int result = Str.UniqueChar(test);
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void UniqueNull()
+        {
+			int result = Str.UniqueChar(null);
+            Assert.AreEqual(-1, result);
         }
     }
 }
--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Text
 {
-    public class Class1
+	/// <summary>Contains multiple methods for managing strings.</summary>
+    public class Str
     {
+		/// <summary>Finds the first unique char in the string.</summary>
+		/// <returns>The first unique char index or -1 if not found.</returns>
+		public static int UniqueChar(string s)
+		{
+			if (s == null)
+				return -1;
+
+			List<char> CharArray = s.ToList();
+			List<char> Removed = new List<char>();
+			char temp = '\0';
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				temp = s[i];
+				CharArray.RemoveAt(0);
+
+				if (!Removed.Contains(temp) && !CharArray.Contains(temp))
+					return i;
+
+				Removed.Add(temp);
+			}
+			return -1;
+		}
     }
 }
