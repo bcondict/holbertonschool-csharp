4563c4f47e6cc1afe512bde995eb5952966a98f6
Jesus <jesus.junc10@gmail.com>
2022-12-20 14:09:38 -0500

    2 task

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        // if (derivedType is baseType)
+        // if (derivedType.GetType() is baseType.GetType())
+        // if ((baseType is derivedType) != null)
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
