80c0b034bfd0faa20602485588f4654c769b5e87
Jesus <jesus.junc10@gmail.com>
2022-12-20 15:42:55 -0500

    4 task

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <sumary> Empty pattern class </sumary>
+class Animal
+{
+    // Empty class
+}
+
+/// <sumary> Empty inherit class </sumary>
+class Dog : Animal
+{
+    // Empty class
+}
