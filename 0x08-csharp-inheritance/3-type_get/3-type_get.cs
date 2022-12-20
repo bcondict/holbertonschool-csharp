a664d2e6dcef0cc44ac29744ad0b07e644039af8
Jesus <jesus.junc10@gmail.com>
2022-12-20 15:29:59 -0500

    3 task

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <sumary> Contains multiple methods for Obj</sumary>
+class Obj
+{
+    /// <sumary> Print object info. </sumary>
+    public static void Print(object myObj)
+    {
+        Console.WriteLine(myObj.GetType().Name + " Properties:");
+        foreach (var property in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        Console.WriteLine(myObj.GetType().Name + "Methods:");
+        foreach (var method in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
