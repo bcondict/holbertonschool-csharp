dd948a3ada2fa0d79adade625f348df5c9100438
Jesus <jesus.junc10@gmail.com>
2022-12-20 13:09:07 -0500

    0 task doc correction

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 
+/// <sumary> contains multiple method for objects </sumary>
 class Obj
 {
+    /// <sumary> check if an object is an int or no </sumary>
+    /// <return> true if <paramref name="obj"/> is int </return>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj in int)
