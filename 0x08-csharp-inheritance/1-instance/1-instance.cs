3b5df67cccaed4417c3c1c9d208a4c53bc04e7bb
Jesus <jesus.junc10@gmail.com>
2022-12-20 13:46:21 -0500

    1 task doc correction

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/// <sumary> contains multiple method for objects </sumary>
 class Obj
 {
+    /// <sumary> check if an object is an array instace or no </sumary>
+    /// <return> true if <paramref name="obj"/> is an array instance</return>
     public static bool IsInstanceOfArray(object obj)
     {
         if (obj is Array)
