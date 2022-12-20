67e02dc80147be6dca5d802b1701ffaf61f9fed5
Jesus <jesus.junc10@gmail.com>
2022-12-20 16:25:28 -0500

    6 task

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+///
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException ("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get { return width; }
+        set { width = value > 0 ? value : throw new ArgumentException ("Width must be greater than or equal to 0"); }
+    }
+
+    public int Height
+    {
+        get { return height; }
+        set { height = value > 0 ? value : throw new ArgumentException ("Heigth must be greater than or equal to 0"); }
+    }
+}
