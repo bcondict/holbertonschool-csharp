ad910296c66e488537bb11cb29aa3997e766b05f
Jesus <jesus.junc10@gmail.com>
2023-01-10 18:54:14 -0500

    9 task

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] fail = new double[] {-1}; 
+        if (vector.Length > 3 || vector.Length < 2)
+            return fail;
+
+        double[] new_vect = new double[vector.Length];
+        for (int i = 0; i < vector.Length; i++)
+            new_vect[i] = vector[i] * scalar;
+
+        return new_vect;
+    }
+}
