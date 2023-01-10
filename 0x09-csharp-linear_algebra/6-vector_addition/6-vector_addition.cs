2f012b174fa95245816df2ba9e9507432365c631
Jesus <jesus.junc10@gmail.com>
2023-01-10 18:03:59 -0500

    6 task

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double x_vect1 = vector1[0];
+        double y_vect1 = vector1[1];
+
+        double x_vect2 = vector2[0];
+        double y_vect2 = vector2[1];
+
+        double[] new_vect = new double[] {x_vect1 + x_vect2, y_vect1 + y_vect2};
+
+        if (vector1.Length == 2 && vector2.Length == 2)
+        {
+            return (new_vect);
+        }
+
+        if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            double z_vect1 = vector1[0];
+            double z_vect2 = vector2[1];
+
+            new_vect = new_vect.Concat(new double[] {z_vect1 + z_vect2});
+
+            return (new_vect);
+        }
+
+        return -1;
+    }
+}
