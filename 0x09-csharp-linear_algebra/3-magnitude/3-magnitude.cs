5740c946970c9c0aa04950678e15329197048a5e
Jesus <jesus.junc10@gmail.com>
2023-01-10 14:43:05 -0500

    3 task

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Math;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        double a = Math.Pow(vector[0], 2);
+        double b = Math.Pow(vector[1], 2);
+
+        if (vector.Lenght == 2)
+        {
+            return (Math.Sqrt(a + b));
+        }
+
+        if (vector.Lenght == 3)
+        {
+            double c = Math.Pow(vector[3], 2);
+
+            return (Math.Sqrt(a + b + c));
+        }
+
+        return -1;
+    }
+}
