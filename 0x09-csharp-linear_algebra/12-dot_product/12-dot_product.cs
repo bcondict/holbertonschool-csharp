70e0886af22c4e9b2c4255a4017d050c80ed15a2
Jesus <jesus.junc10@gmail.com>
2023-01-10 19:13:26 -0500

    12 task correction

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -8,9 +8,9 @@ class VectorMath
             return -1;
 
         if (vector1.Length > 3 || vector1.Length < 2)
-            return -1
+            return -1;
 
-        double[] new_vect = new double[vector.Length];
+        double[] new_vect = new double[vector1.Length];
         double result = 0;
 
         for (int i = 0; i < vector1.Length; i++)
