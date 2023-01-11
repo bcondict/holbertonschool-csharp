2e0c7934ba77da2d39f7705564bf2a8d7bcea8a2
Jesus <jesus.junc10@gmail.com>
2023-01-11 16:18:59 -0500

    3 task jerson's way

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -8,25 +8,24 @@ class VectorMath
     /// <summary>
     /// calculates and returns the lenght of a given vector
     /// </summary>
-    /// <param name="vector"></param>
+    /// <param name="vector">Vector to find the magnitud</param>
     /// <returns>Magnitud of a given vector</returns>
     public static double Magnitude(double[] vector)
     {
-        double a = Math.Pow(vector[0], 2);
         double b = Math.Pow(vector[1], 2);
 
         if (vector.Length == 2)
         {
-            return (Math.Round(Math.Sqrt(a + b), 2));
+            double res = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
+            return (Math.Round(Math.Sqrt(res), 2));
         }
 
         if (vector.Length == 3)
         {
-            double c = Math.Pow(vector[2], 2);
-
-            return (Math.Round(Math.Sqrt(a + b + c), 2));
+            double res = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
+            return (Math.Round(Math.Sqrt(res), 2));
         }
 
-        return (-1);
+        return -1;
     }
 }
