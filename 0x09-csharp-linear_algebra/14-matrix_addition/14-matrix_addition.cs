e7d8178eb57a14cfe0e1c8480eb848e95f585530
Jesus <jesus.junc10@gmail.com>
2023-01-11 17:59:39 -0500

    14 task

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        // matrix that contain -1 value to say it fails
+        double[,] fail = new double[,] { { -1 } };
+
+        // to check if matrix1 have a different size than matrix 2
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) || (matrix1.GetLength(1) != matrix2.GetLength(1)))
+            return fail;
+
+        if ((matrix1.GetLength(0) > 3 || matrix1.GetLength(0) < 2) || (matrix1.GetLength(1) > 3 || matrix1.GetLength(1) < 2))
+            return fail;
+
+        // to check if matrix has same size, if it's 2d or 3d valid
+        if (matrix1.GetLength(0) != matrix1.GetLength(1) || matrix2.GetLength(0) != matrix2.GetLength(1))
+            return fail;
+
+        double[,] result_matrix = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+
+        for (int row = 0; row < matrix1.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix1.GetLength(1); col++)
+                result_matrix[row, col] = matrix1[row, col] + matrix2[row, col];
+        }
+
+        return result_matrix;
+    }
+}
