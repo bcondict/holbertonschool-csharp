1ec031f09085f8f07e4c32e7be451688f7870feb
Jesus <jesus.junc10@gmail.com>
2023-01-11 16:37:31 -0500

    3 task my way without braces

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -12,18 +12,19 @@ class VectorMath
     /// <returns>Magnitud of a given vector</returns>
     public static double Magnitude(double[] vector)
     {
+        double a = Math.Pow(vector[0], 2);
         double b = Math.Pow(vector[1], 2);
 
         if (vector.Length == 2)
         {
-            double res = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
-            return (Math.Round(Math.Sqrt(res), 2));
+            return (Math.Round(Math.Sqrt(a + b), 2));
         }
 
         if (vector.Length == 3)
         {
-            double res = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
-            return (Math.Round(Math.Sqrt(res), 2));
+            double c = Math.Pow(vector[2], 2);
+
+            return (Math.Round(Math.Sqrt(a + b + c), 2));
         }
 
         return -1;
