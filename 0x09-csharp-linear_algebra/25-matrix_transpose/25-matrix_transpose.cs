6994e0ae8cde984e39f42ec401257dcba90053a3
Jesus <jesus.junc10@gmail.com>
2023-01-12 15:52:45 -0500

    25 task Transpose

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] result_matrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        if (matrix.GetLength(0) == 0)
+            return result_matrix;
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                result_matrix[col, row] = matrix[row, col];
+            }
+        }
+
+        return result_matrix;
+    }
+}
