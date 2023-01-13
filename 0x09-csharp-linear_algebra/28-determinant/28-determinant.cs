3204a907cfb8e2d9f1081a878f48e2860e0d5fc6
Jesus <jesus.junc10@gmail.com>
2023-01-13 15:15:37 -0500

    28 task determinant correction

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -16,20 +16,20 @@ class MatrixMath
             return fail;
 
         double determinatn_val = 0;
-        if (matrix.GetLength(0) == 1)
+        if (matrix.GetLength(0) == 2)
         {
             determinatn_val = matrix[0, 0] * matrix[1, 1] + matrix[1, 0] * matrix[0, 1];
             return Math.Round(determinatn_val); 
         }
 
-        if (matrix.GetLength(0) == 2)
+        if (matrix.GetLength(0) == 3)
         {
             double first_val = 0;
             double second_val = 0;
             double third_val = 0;
 
             first_val = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[2, 1] * matrix[1, 2]);
-            second_val = matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 2]);
+            second_val = matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[2, 0] * matrix[1, 2]);
             third_val = matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[2, 0] * matrix[1, 1]);
 
             determinatn_val = first_val - second_val + third_val;
