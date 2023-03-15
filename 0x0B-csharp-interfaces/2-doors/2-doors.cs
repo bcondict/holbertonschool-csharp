c24d0426c8cfcf3382b2f658f3c6ecbb5a0ef64d
Jesus <jesus.junc10@gmail.com>
2023-03-15 18:27:14 -0500

    Interact method corrected string

--- 0x0B-csharp-interfaces/2-doors/2-doors.cs
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -85,6 +85,6 @@ public class Door : Base, IInteractive
     /// </summary>
     public void Interact()
     {
-        Console.WriteLine($"You try to open the {name}. It's locked");
+        Console.WriteLine($"You try to open the {name}. It's locked.");
     }
 }
